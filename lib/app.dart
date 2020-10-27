769219c6d9b8a0512cf8e7cb83250a8f07a00009
antklim <antklim@gmail.com>
2020-10-27 22:18:32 +1100

    Bootstrap cleanup continue

---
+++ lib/app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Calculus extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CalculusScreen(),
+    );
+  }
+}
+
+class CalculusScreen extends StatefulWidget {
+  @override
+  _CalculusScreenState createState() => _CalculusScreenState();
+}
+
+class _CalculusScreenState extends State<CalculusScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
